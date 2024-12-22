58bbb1aefd8e216507fbf3915f205e8cc6c2e51d
TruongAn <truonganspkt2014@gmail.com>
2024-12-22 15:03:35 +0700

    responsive_header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,30 @@ module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
     extend: {
+      screens: {
+        sm: '576px',
+      },
       colors: {
         'light-coffee': '#C89F94',
+      },
+
+      fontFamily: {
+        Karla: ['Karla', 'serif'],
+      },
+
+      keyframes: {
+        slideDown: {
+          '0%': { transform: ' translateY(-100%)' },
+          '100%': { transform: ' translateY(0%)' },
+        },
+        expandMenu: {
+          '0%': { transform: 'scaleY(0)', transformOrigin: 'top' },
+          '100%': { transform: 'scaleY(1)', transformOrigin: 'top' },
+        },
+      },
+      animation: {
+        slideDown: 'slideDown .4s ease-in-out ',
+        expandMenu: 'expandMenu .4s ease ',
       },
     },
   },
